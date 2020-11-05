ded2c8ca75faa7650caadf33dc0881f065eb2eb1
Game Over <janessarefong@gmail.com>
2020-11-04 23:54:09 -0800

    edit api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,21 +1,19 @@
-var db = require("../models");
-
 // Routes
 module.exports = function(app) {
 
-    app.get("/api/contacts", function(req, res) {
-        db.Contact.findAll({}).then(function(dbContact) {
-            res.json(dbContact);
-        });
-    });
+    // app.get("/api/contacts", function(req, res) {
+    //     db.Contact.findAll({}).then(function(dbContact) {
+    //         res.json(dbContact);
+    //     });
+    // });
 
-    app.post("/api/contacts", function(req, res) {
-        db.Contact.create({
-            name: req.body.name,
-            email: req.body.email,
-            message: req.body.message
-        }).then(function(dbContact) {
-            res.json(dbContact);
-        });
-    });
+    // app.post("/api/contacts", function(req, res) {
+    //     db.Contact.create({
+    //         name: req.body.name,
+    //         email: req.body.email,
+    //         message: req.body.message
+    //     }).then(function(dbContact) {
+    //         res.json(dbContact);
+    //     });
+    // });
 };
